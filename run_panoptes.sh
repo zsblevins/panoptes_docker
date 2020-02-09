7ff752baef81acb837956b59bb83c152d79d0e92
cawoodfield <chris@semihuman.com>
2020-02-09 15:11:07 -0800

    fixed polling pluigin location/mount

--- run_panoptes.sh
+++ run_panoptes.sh
@@ -18,7 +18,7 @@ docker run --rm -d \
     --sysctl net.core.somaxconn=511 \
     --name="panoptes_docker" \
     --shm-size=2G \
-    -v ${mount}/plugins/polling:/home/panoptes_v/lib/python3.6/site-packages/yahoo_panoptes/plugins/polling/${mount_options} \
+    -v ${mount}/plugins/polling/napalm:/home/panoptes_v/lib/python3.6/site-packages/yahoo_panoptes/plugins/polling/napalm/${mount_options} \
     -v ${mount}/conf/localhost.json:/home/panoptes/conf/localhost.json${mount_options} \
     -p 127.0.0.1:8080:3000/tcp \
     panoptes_docker
