395cd6f756f66c15d597816e5a791e26cccbaf31
Zoe Blevins <zblevins@netflix.com>
2020-02-11 09:41:48 -0800

    add linkrel data to relationships

--- resources/listeners/kafka_listener.py
+++ resources/listeners/kafka_listener.py
@@ -9,16 +9,19 @@ config.DATABASE_URL = 'bolt://neo4j:test@host.docker.internal:7687'
 
 
 class LinkRel(StructuredRel):
-    local_port = StringProperty()
-    remote_port = StringProperty()
+    last_seen = DateTimeProperty(default=lambda: datetime.now())
     input_rate = IntegerProperty()
     output_rate = IntegerProperty()
     speed = IntegerProperty()
+    local_port = StringProperty()
+    remote_port = StringProperty()
 
 
 class Router(StructuredNode):
     name = StringProperty()
-    connects = RelationshipTo('Router', 'CONNECTS')
+    vendor = StringProperty()
+    connects = RelationshipTo('Router', 'CONNECTS', model=LinkRel)
+
 
 def main():
     consumer = KafkaConsumer('local-processed', bootstrap_servers=['localhost:9092'], group_id='topology')
@@ -26,7 +29,6 @@ def main():
     while True:
         for message in consumer:
             metric = json.loads(message.value.decode())
-
 
             dimensions = {dim['dimension_name']: dim['dimension_value'] for dim in metric['dimensions']}
             metrics = {m['metric_name'] + '_' + m['metric_type']: m['metric_value'] for m in metric['metrics']}
@@ -42,45 +44,35 @@ def main():
                 output_rate = metrics.get('output_rate_gauge', 0)
                 speed = dimensions['speed']
 
+                if local_port in ['Management1', 'mgmt0'] or remote_port in ['Management1', 'mgmt0']:
+                    continue
 
-                try:
-                    router_a = Router.nodes.get(name=local_router)
-                except:
-                    router_a = Router(name=local_router).save()
+                with db.transaction:
+                    router_a = Router.nodes.get_or_none(name=local_router)
+                    if not router_a:
+                        router_a = Router(name=local_router).save()
 
-                try:
-                    router_z = Router.nodes.get(name=remote_router)
-                except:
-                    router_z = Router(name=remote_router).save()
+                    router_z = Router.nodes.get_or_none(name=remote_router)
+                    if not router_z:
+                        router_z = Router(name=remote_router).save()
 
-                # links = router_a.connects.match(local_port=local_port)
-                # for link in links:
-                #     link.delete()
-                #
-                # links = router_z.connects.match(local_port=remote_port)
-                # for link in links:
-                #     link.delete()
+                    router_a.connects.disconnect(router_z)
+                    router_z.connects.disconnect(router_a)
+                    router_a.connects.connect(router_z, {
+                        'local_port': local_port,
+                        'remote_port': remote_port,
+                        'input_rate': input_rate,
+                        'output_rate': output_rate,
+                        'speed': speed
+                    })
 
-
-                router_a.connects.connect(router_z)
-                router_z.connects.connect(router_a)
-
-                # router_a.connects.connect(router_z, {
-                #     'input_rate': int(input_rate),
-                #     'output_rate': int(output_rate),
-                #     'speed': int(speed),
-                #     'local_port': local_port,
-                #     'remote_port': remote_port
-                # })
-
-                # router_z.connects.connect(router_a, {
-                #     'input_rate': int(output_rate),
-                #     'output_rate': int(input_rate),
-                #     'speed': int(speed),
-                #     'local_port': remote_port,
-                #     'remote_port': local_port
-                # })
-
+                    router_z.connects.connect(router_a, {
+                        'local_port': remote_port,
+                        'remote_port': local_port,
+                        'input_rate': output_rate,
+                        'output_rate': input_rate,
+                        'speed': speed
+                    })
 
 
 if __name__ == '__main__':
